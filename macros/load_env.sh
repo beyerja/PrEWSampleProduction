0bd09a772a3035e76debdb98770c739e9bc452dd
Jakob Beyer <beyerjac@flc17.desy.de>
2020-10-01 18:15:12 +0200

    Make environment loading more verbose
    - Make environment loading more verbose.

--- macros/load_env.sh
+++ macros/load_env.sh
@@ -5,7 +5,8 @@ echo "### Loading needed software versions ####################################"
 echo "#########################################################################"
 
 # Load up-to-date iLCSoft 
-ilcsoft_dir="/cvmfs/ilc.desy.de/sw/x86_64_gcc82_sl6/v02-02"
+ilcsoft_version="v02-02"
+ilcsoft_dir="/cvmfs/ilc.desy.de/sw/x86_64_gcc82_sl6/${ilcsoft_version}"
 
 if [[ "${ILCSOFT}" == *"${ilcsoft_dir}"* ]]; then
   echo "iLCSoft version is already loaded."
@@ -14,6 +15,7 @@ elif [ ! -z "${ILCSOFT}" ]; then
   echo "ERROR: Another iLCSoft version is already loaded!"
   echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 else 
+  echo "Loading iLCSoft - version: ${ilcsoft_version}"
   source ${ilcsoft_dir}/init_ilcsoft.sh
 fi
 
