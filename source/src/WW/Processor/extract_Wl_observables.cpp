3f83f7ac0e08b47efa433db44ed57e2d0f934489
Jakob Beyer <beyerja@gmx.de>
2020-11-02 19:04:22 +0100

    Switch order of boost and rotation in WW
    - Switch order of boost and rotation in WW observable calculation for 
    readabilities sake.

--- source/src/WW/Processor/extract_Wl_observables.cpp
+++ source/src/WW/Processor/extract_Wl_observables.cpp
@@ -21,21 +21,25 @@ void WWProcessor::extract_Wl_observables(const EVENT::MCParticle &Wl,
   // Lorentz vectors in e+e- frame (after removing crossing angle)
   auto energy = m_header.m_energy;
   auto eM_tlv_ee = TLorentzVector(0, 0, energy, energy); // e- in z direction
-  
+
+  auto l_tlv_ee = l_tlv_lab;
   auto Wl_tlv_ee = Wl_tlv_lab;
   if (m_unboost_xangle) {
+    l_tlv_ee =
+        MarlinHelp::ILD::Machine::unboost_crossing_angle(l_tlv_lab, energy);
     Wl_tlv_ee =
         MarlinHelp::ILD::Machine::unboost_crossing_angle(Wl_tlv_lab, energy);
   }
 
-  // Lorentz vectors in W_l system
-  auto l_tlv_Wl =
-      MarlinHelp::Root::LorentzVec::boost_tlv(l_tlv_lab, Wl_tlv_lab);
+  // Rotate to be along W flight direction (x in e-W plane, z along W flight)
+  auto l_tlv_ee_rot =
+      MarlinHelp::Root::LorentzVec::rotate_into(l_tlv_ee, Wl_tlv_ee, eM_tlv_ee);
+  auto Wl_tlv_ee_rot = MarlinHelp::Root::LorentzVec::rotate_into(
+      Wl_tlv_ee, Wl_tlv_ee, eM_tlv_ee);
 
-  // Rotate to be along W flight direction (x in e-W plane)
-  // (Previous boost was along W direction, so e-W plane didn't change)
+  // Lorentz vectors in W_l system
   auto l_tlv_Wl_rot =
-      MarlinHelp::Root::LorentzVec::rotate_into(l_tlv_Wl, Wl_tlv_ee, eM_tlv_ee);
+      MarlinHelp::Root::LorentzVec::boost_tlv(l_tlv_ee_rot, Wl_tlv_ee_rot);
 
   // --- Find observables ------------------------------------------------------
   m_observables.costh_l_star = l_tlv_Wl_rot.CosTheta();
