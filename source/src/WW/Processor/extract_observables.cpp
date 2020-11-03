9d24863642e0692c915cc24077d43299548d8b4d
Jakob Beyer <beyerja@gmx.de>
2020-11-03 14:14:43 +0100

    Add WW processor MCParticle debugging
    - Add MCParticle print function.
    - Use MCParticle print function for debugging in the WW processor.

--- source/src/WW/Processor/extract_observables.cpp
+++ source/src/WW/Processor/extract_observables.cpp
@@ -1,13 +1,17 @@
 #include <Utils/MC.h>
 #include <WW/WWProcessor.h>
 
+// Includes from iLCSoft
+#include "streamlog/streamlog.h"
+
 void WWProcessor::extract_observables(const EVENT::MCParticleVec &mcps) {
   /** Extract the relevant generator level observables.
    **/
   // Find the first lepton (skip the initial e+e- and ISR -> skip 8)
   auto l = Utils::MC::find_first_lepton(mcps, 8);
-  m_observables.l_charge = int(l->getCharge());
+  streamlog_out(DEBUG) << "Found l: " << Utils::MC::print(*l) << std::endl; 
 
+  m_observables.l_charge = int(l->getCharge());
   if (Utils::MC::is_mu(*l))
     m_observables.decay_to_mu = true;
   if (Utils::MC::is_tau(*l))
@@ -28,6 +32,9 @@ void WWProcessor::extract_observables(const EVENT::MCParticleVec &mcps) {
     W_h_tech = Utils::MC::determine_W(mcps, -1 * m_observables.l_charge);
     W_h = &W_h_tech;
   }
+
+  streamlog_out(DEBUG) << "Found W_l: " << Utils::MC::print(*W_l) << std::endl; 
+  streamlog_out(DEBUG) << "Found W_h: " << Utils::MC::print(*W_h) << std::endl; 
 
   // Which one is W+, which one W-?
   auto Wplus = W_l;
